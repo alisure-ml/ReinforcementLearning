ef219d9f6db0d9b35f582195e9c5b81aacc561e3
ALISURE <562282219@qq.com>
2017-10-25 16:28:03 +0800

    game list

--- src/gym_1_TestGym.py
+++ src/gym_1_TestGym.py
@@ -2,7 +2,21 @@ import gym
 
 # env = gym.make("CartPole-v0")
 # env = gym.make("MountainCar-v0")
-env = gym.make("FrozenLake-v0")
+# env = gym.make("FrozenLake-v0")
+# env = gym.make("AirRaid-v0")
+# env = gym.make("Alien-v0")
+# env = gym.make("Amidar-v0")
+# env = gym.make("Ant-v1")
+# env = gym.make("Assault-v0")
+# env = gym.make("Asterix-v0")
+# env = gym.make("Atlantis-v0")
+# env = gym.make("BankHeist-v0")
+# env = gym.make("BattleZone-v0")
+# env = gym.make("BeamRider-v0")
+env = gym.make("Berzerk-v0")
+
+
+
 
 env.reset()
 for _ in range(1000):
